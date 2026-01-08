a26b25b176947291da24bf3de39a48cb32b5d9da
Matthieu Felten <matthieu.felten22@gmail.com>
2026-01-08 15:29:58 +0100

    Add selection mode

--- TD6/Exercice7/grid.py
+++ TD6/Exercice7/grid.py
@@ -15,6 +15,8 @@ class Grid(Widget):
         self.cols, self.rows = self.game.GRID_SIZE
         self.draw_value = None
         self.do_edit = False
+        self.selection_mode = False
+        self.selection = [None, None]
         self.has_after = False
 
         self.texture = Texture.create(size=self.game.GRID_SIZE, colorfmt="rgba")
@@ -22,6 +24,13 @@ class Grid(Widget):
         self.texture.min_filter = "nearest"
 
         self.game.bind(on_update=self.update_display)
+
+    def get_cell_pos(self, x, y):
+        cell_x = int(x // self.cell_size)
+        cell_y = int(y // self.cell_size)
+        if 0 <= cell_x < self.cols and 0 <= cell_y < self.rows:
+            return cell_x, cell_y
+        return None, None
 
     # ------------------------------
     # Events
@@ -31,36 +40,48 @@ class Grid(Widget):
         if not self.do_edit or touch.is_mouse_scrolling:
             return super().on_touch_down(touch)
 
-        x = int(touch.x // self.cell_size)
-        y = int(touch.y // self.cell_size)
+        x, y = self.get_cell_pos(*touch.pos)
 
-        if 0 <= x < self.cols and 0 <= y < self.rows:
-            self.draw_value = self.game.get_cell(x, y) ^ 1
-            self.game.set_cell(x, y, self.draw_value)
-            self.last_cell = (x, y)
+        if x is not None:
+            if self.selection_mode:
+                if touch.button == "right":
+                    self.selection = [None, None]
+                else:
+                    self.selection = [[x, y], None]
+                self.update_selection()
+            else:
+                self.draw_value = self.game.get_cell(x, y) ^ 1
+                self.game.set_cell(x, y, self.draw_value)
+                self.last_cell = (x, y)
             return True
 
         return super().on_touch_down(touch)
 
     def on_touch_move(self, touch):
-        if not self.do_edit or self.draw_value is None:
+        if not self.do_edit:
             return super().on_touch_move(touch)
 
-        x = int(touch.x // self.cell_size)
-        y = int(touch.y // self.cell_size)
+        x, y = self.get_cell_pos(*touch.pos)
 
-        if 0 <= x < self.cols and 0 <= y < self.rows:
-            if self.last_cell is not None:
-                self.game.set_cell(x, y, self.draw_value)
-                self.game.draw_line(self.last_cell, (x, y), self.draw_value)
-            self.last_cell = (x, y)
+        if x is not None:
+            if self.selection_mode and self.selection[0] is not None:
+                self.selection[1] = [x, y]
+                self.update_selection()
+            elif self.draw_value is not None:
+                if self.last_cell is not None:
+                    self.game.set_cell(x, y, self.draw_value)
+                    self.game.draw_line(self.last_cell, (x, y), self.draw_value)
+                self.last_cell = (x, y)
+            else:
+                return super().on_touch_move(touch)
             return True
-
         return super().on_touch_move(touch)
 
     def on_touch_up(self, touch):
         self.last_cell = None
         self.draw_value = None
+        if self.selection_mode:
+            self.update_selection()
         return super().on_touch_up(touch)
 
     # ------------------------------
@@ -86,20 +107,49 @@ class Grid(Widget):
             Color(1, 1, 1)
             Rectangle(texture=self.texture, pos=self.pos, size=self.size)
 
+        # Update the selection
+        self.update_selection()
+
         # Update black grid
         self.update_black_grid(scale)
+
+    def update_selection(self):
+        self.canvas.after.clear()
+        if self.selection_mode and self.selection[1] is not None:
+            x1 = self.x + self.selection[0][0] * self.cell_size
+            y1 = self.y + self.selection[0][1] * self.cell_size
+            x2 = self.x + self.selection[1][0] * self.cell_size
+            y2 = self.y + self.selection[1][1] * self.cell_size
+
+            xmin = min(x1, x2)
+            xmax = max(x1, x2) + self.cell_size
+            ymin = min(y1, y2)
+            ymax = max(y1, y2) + self.cell_size
+
+            with self.canvas.after:
+                Color(1, 0, 0)
+                Line(
+                    points=(
+                        (xmin, ymin),
+                        (xmin, ymax),
+                        (xmax, ymax),
+                        (xmax, ymin),
+                        (xmin, ymin),
+                    ),
+                    width=1,
+                )
 
     def update_black_grid(self, scale):
         if scale is None:
             return
         # Clear or not the black grid
         if scale < 1.5 and self.has_after:
-            self.canvas.after.clear()
+            self.canvas.clear()
             self.has_after = False
             return
         # Draw of not the black grid
         if scale >= 1.5 and not self.has_after:
-            with self.canvas.after:
+            with self.canvas:
                 self.has_after = True
                 Color(0, 0, 0)
                 for x in range(self.game.GRID_SIZE[0] + 1):
